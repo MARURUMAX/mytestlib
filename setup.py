eea3fab98ea8c7be51d174dc15159fdf92fa2dd9
MARURUMAX <61178526+MARURUMAX@users.noreply.github.com>
2020-02-18 15:49:31 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-…',
+    version='1.0.0',
+    url='…',
+    author='…',
+    author_email='…',
+    description='My Test Library.',
+    packages=find_packages()
+)
